9e38e56d2409c57e6bb9770cc379f46327b4093d
Vishal Chauhan <lallanchouhan665@gmail.com>
2026-09-11 11:04:51 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿const API_BASE_URL = "http://127.0.0.1:8000";
+﻿const API_BASE_URL = "https://mental-health-score-z107.onrender.com";
 
 const form = document.querySelector("#prediction-form");
 const predictButton = document.querySelector("#predict-button");
